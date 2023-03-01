5446341fdf7218392f5ef099722557ca63d5a8be
Igor G <i.gorbatenco@gmail.com>
2023-03-01 00:49:10 -0500

    added subtraction function

--- funcs.py
+++ funcs.py
@@ -3,3 +3,10 @@ def sum_numbers(numbers):
     for number in numbers:
         total += number
     return total
+
+
+def diff_numbers(numbers):
+    total1 = 0
+    for number in numbers:
+        total1 -= number
+    return total1
